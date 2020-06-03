96b4e7414837a13731ea6c9d3de56d9ea4e11b61
Georgette Class-Peters <georgycp@udel.edu>
2020-06-03 22:13:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,20 +9,11 @@ class School
   end 
   
   def add_student(name, grade)
-    @roster[grade] ||=[ ]
-    @roster[grade] << name 
-  end 
-  def 
+     
     self.roster[grade] ||=[]
     self.roster[grade] << name 
   end 
   
-  
-  
-  
-  
-  
-   
    def grade(number) 
      roster[number]
   end 
